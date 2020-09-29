91891732b9972ec4ffde41714aa0810b912dd334
vladjk <33227105+vladjk@users.noreply.github.com>
2020-09-29 17:33:19 +0200

    Improved data retrieving with firebase. Now supports multiple tables.

---
+++ app/src/main/java/com/example/eventbrowser/Feed.java
@@ -0,0 +1,72 @@
+package com.example.eventbrowser;
+
+public class Feed {
+    private String name;
+    private String map;
+    private String loc;
+    private String desc;
+    private String date;
+    private String image;
+
+    public Feed(String name, String map, String loc, String desc, String date,String image) {
+        this.name = name;
+        this.map = map;
+        this.loc = loc;
+        this.desc = desc;
+        this.date = date;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMap() {
+        return map;
+    }
+
+    public void setMap(String map) {
+        this.map = map;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Feed(){
+
+    }
+
+
+}
--- app/src/main/java/com/example/eventbrowser/MainActivity.java
+++ app/src/main/java/com/example/eventbrowser/MainActivity.java
@@ -2,23 +2,31 @@ package com.example.eventbrowser;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
     TextView a,b,c,d,e;
     Button btn;
-    DatabaseReference reff;
+    DatabaseReference eventDatabase;
+    RecyclerView newsfeedList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +34,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Firebase connection established", Toast.LENGTH_SHORT).show();
 
-        a = (TextView) findViewById(R.id.txtname);
-        b = (TextView) findViewById(R.id.txtloc);
-        c = (TextView) findViewById(R.id.txtmap);
-        d = (TextView) findViewById(R.id.txtdesc);
-        e = (TextView) findViewById(R.id.txtdate);
-        btn= (Button) findViewById(R.id.btnGetData);
+        eventDatabase = FirebaseDatabase.getInstance().getReference().child("Event");
+        eventDatabase.keepSynced(true);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        newsfeedList = (RecyclerView) findViewById(R.id.recycleViewFeed);
+        newsfeedList.setHasFixedSize(true);
+        newsfeedList.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Feed,FeedViewHolder>firebaseRecyclerAdapter=new FirebaseRecyclerAdapter<Feed, FeedViewHolder>
+                (Feed.class,R.layout.eventcard,FeedViewHolder.class, eventDatabase) {
             @Override
-            public void onClick(View view) {
-                reff = FirebaseDatabase.getInstance().getReference().child("Event").child("1");
-                reff.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        String name = snapshot.child("name").getValue().toString();
-                        String map = snapshot.child("map").getValue().toString();
-                        String loc = snapshot.child("loc").getValue().toString();
-                        String desc = snapshot.child("desc").getValue().toString();
-                        String date = snapshot.child("date").getValue().toString();
-                        a.setText(name);
-                        b.setText(map);
-                        c.setText(loc);
-                        d.setText(desc);
-                        e.setText(date);
-                    }
+            protected void populateViewHolder(FeedViewHolder feedViewHolder, Feed feed, int i) {
+            feedViewHolder.setName(feed.getName());
+            feedViewHolder.setDesc(feed.getDesc());
+            feedViewHolder.setImage(getApplicationContext(),feed.getImage());
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
 
-                    }
-                });
             }
-        });
+        };
+
+        newsfeedList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class FeedViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        public FeedViewHolder(View itemView){
+            super(itemView);
+            mView=itemView;
+        }
+        public void setName(String name){
+            TextView post_name = (TextView)mView.findViewById(R.id.post_name);
+            post_name.setText(name);
+        }
+        public void setDesc(String desc){
+            TextView post_desc = (TextView)mView.findViewById(R.id.post_desc);
+            post_desc.setText(desc);
+        }
+        public void setImage(Context ctx,String image){
+            ImageView post_image=(ImageView)mView.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image);
+
+        }
     }
 }
