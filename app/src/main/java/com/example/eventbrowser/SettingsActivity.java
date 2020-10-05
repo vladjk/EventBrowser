d8dab8e30c32051c8ad48ecc930bea51938894ee
vladjk <33227105+vladjk@users.noreply.github.com>
2020-10-05 20:23:32 +0200

    Added functionality for Network connectivity testing in SettingsActivity.

--- app/src/main/java/com/example/eventbrowser/SettingsActivity.java
+++ app/src/main/java/com/example/eventbrowser/SettingsActivity.java
@@ -3,9 +3,17 @@ package com.example.eventbrowser;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button networkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,30 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setTitle("Settings");
+
+        /** Checks connection status on button click */
+        networkBtn = findViewById(R.id.networkButton);
+        networkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isNetworkAvailable() == true){
+                    Toast.makeText(SettingsActivity.this, "Connection OK", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SettingsActivity.this, "Connection FAIL", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
+    /** Retrieves active connection info */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+
 }
