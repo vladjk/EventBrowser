18a1d171c0c47123fe862212aa00859dce51aea0
vladjk <33227105+vladjk@users.noreply.github.com>
2020-11-19 14:49:09 +0100

    Added location availability checker in SettingsActivity.

--- app/src/main/java/com/example/eventbrowser/SettingsActivity.java
+++ app/src/main/java/com/example/eventbrowser/SettingsActivity.java
@@ -2,23 +2,33 @@ package com.example.eventbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.location.LocationManagerCompat;
 
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Set;
+
 public class SettingsActivity extends AppCompatActivity {
 
     Button networkBtn;
+    Button locationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
 
         /** Reference toolbar **/
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -39,6 +49,18 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        locationBtn = findViewById(R.id.locationButton);
+        locationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isLocationAvailable(SettingsActivity.this)== true){
+                    Toast.makeText(SettingsActivity.this,"Location OK", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(SettingsActivity.this,"Location Unavailable", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
     /** Retrieves active connection info */
@@ -46,6 +68,11 @@ public class SettingsActivity extends AppCompatActivity {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static boolean isLocationAvailable(Context context){
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return LocationManagerCompat.isLocationEnabled(locationManager);
     }
 
 
